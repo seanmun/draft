7bb9fa484f98572e97ca35273592d8b8664e5cdf
seanmun <smunley13@gmail.com>
2021-04-29 17:48:37 -0400

    first

---
+++ assets/javascript/results.js
@@ -0,0 +1,21 @@
+
+// Wait for page to load
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	ready();
+})
+
+function ready() {
+	let url = 'https://api.sheety.co/b2d25a34b81ecc6a5b3557adc04eb45a/2021NflDraftPool/scoreboard';
+	fetch(url)
+	.then((response) => response.json())
+	.then(json => {
+		// Group menu items by their category
+		let groupedMenu = _.groupBy(json.scoreboard, 'category');
+		// Create a Handlebars template to render items
+		var template = Handlebars.compile(document.getElementById("menu-template").innerHTML);
+		// Render items into Handlebars template, and set the html of the container element
+		document.getElementById('menu').innerHTML = template(groupedMenu);
+	});
+}
+
