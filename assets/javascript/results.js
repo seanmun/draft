20c1fe8aedde8825980b83075b57f36e689757b4
seanmun <smunley13@gmail.com>
2022-04-22 23:52:59 -0400

    updates

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
 })
 
 function ready() {
-	let url = 'https://api.sheety.co/b2d25a34b81ecc6a5b3557adc04eb45a/2022NflDraftPool/scoreboard';
+	let url = 'https://api.sheety.co/b2d25a34b81ecc6a5b3557adc04eb45a/2021NflDraftPool/scoreboard';
 	fetch(url)
 	.then((response) => response.json())
 	.then(json => {
 		// Group menu items by their category
-		let groupedMenu = _.groupBy(json.scoreboard, 'category');
+		let groupedMenu = _.groupBy(json.scoreboard, 'Category');
 		// Create a Handlebars template to render items
 		var template = Handlebars.compile(document.getElementById("menu-template").innerHTML);
 		// Render items into Handlebars template, and set the html of the container element
