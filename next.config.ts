c64fc5ecb50f67e19206a9bd44050726ea3aa62f
seanmun <smunley13@gmail.com>
2025-04-16 01:13:21 -0400

    congif

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disabling ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
